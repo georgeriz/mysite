1b69734ca21e16f81acfa72d9d25e3c94db3b2fe
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-01-15 20:42:17 +0100

    model for nps answer

--- nps/models.py
+++ nps/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Answer(models.Model):
+	# TODO answer_text should be an integer
+	# and a restricted option
+	answer_text = models.CharField(max_length=10)
+	feedback_text = models.CharField(max_length=200)
+	mail_text = models.CharField(max_length=200)
+	user_text = models.CharField(max_length=200)
+	answer_date = models.DateTimeField()
