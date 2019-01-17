384323b6173f0e5e65e2c6263c0a0abfdefbf32c
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-01-17 19:15:13 +0100

    added thanks view and url

--- nps/urls.py
+++ nps/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-	url(r'^submit/$', views.submit_answer, name='submit_answer')
+	url(r'^submit/$', views.submit_answer, name='submit_answer'),
+	url(r'^thanks/$', views.thanks, name='thanks')
 ]
