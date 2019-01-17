5009f531b6ac1e19226e462e01f72aeaa41942a1
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-01-17 19:54:02 +0100

    answer is restricted to preset

--- nps/models.py
+++ nps/models.py
@@ -7,16 +7,25 @@ from django.db import models
 
 @python_2_unicode_compatible
 class Answer(models.Model):
-	# TODO answer_text should be an integer
-	# and a restricted option
-	answer_text = models.CharField(max_length=10)
+	choices = [
+		(1,1),
+		(2,2),
+		(3,3),
+		(4,4),
+		(5,5),
+		(6,6),
+		(7,7),
+		(8,8),
+		(9,9),
+		(10,10)]
+	answer_text = models.IntegerField(choices=choices)
 	feedback_text = models.CharField(max_length=200)
 	mail_text = models.CharField(max_length=200)
 	user_text = models.CharField(max_length=200)
 	answer_date = models.DateTimeField()
 	
 	def __str__(self):
-		return ', '.join([self.answer_text, self.feedback_text, str(self.answer_date)])
+		return ', '.join([str(self.answer_text), self.feedback_text, str(self.answer_date)])
 	
 class AnswerForm(ModelForm):
     class Meta:
